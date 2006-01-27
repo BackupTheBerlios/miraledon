4a5aac31a6660e6fa7e83bd342800f877f72e47d
gerrit-albrecht <gerrit-albrecht>
2006-01-27 12:15:43 +0000

    *** empty log message ***

---
+++ Miraledon/Miraledon.cpp
@@ -0,0 +1,102 @@
+// $Id: Miraledon.cpp,v 1.1 2006/01/27 12:15:43 gerrit-albrecht Exp $
+//
+// Miraledon
+// Copyright (C) 2006 by Gerrit M. Albrecht
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+
+/// \file Miraledon.cpp
+/// \author Gerrit M. Albrecht
+/// \brief Defines the initialization routines for the DLL.
+
+#include "StdAfx.h"
+
+#include <afxdllx.h>
+
+#ifdef _MANAGED
+#error Please read instructions in Miraledon.cpp to compile with /clr
+// If you want to add /clr to your project you must do the following:
+//	1. Remove the above include for afxdllx.h
+//	2. Add a .cpp file to your project that does not have /clr thrown and has
+//	   Precompiled headers disabled, with the following text:
+//			#include <afxwin.h>
+//			#include <afxdllx.h>
+#endif
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+static AFX_EXTENSION_MODULE MiraledonDLL = { NULL, NULL };
+
+#ifdef _MANAGED
+#pragma managed(push, off)
+#endif
+
+extern "C" int APIENTRY
+DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	// Remove this if you use lpReserved
+	UNREFERENCED_PARAMETER(lpReserved);
+
+	if (dwReason == DLL_PROCESS_ATTACH)
+	{
+		TRACE0("Miraledon.DLL Initializing!\n");
+		
+		// Extension DLL one-time initialization
+		if (!AfxInitExtensionModule(MiraledonDLL, hInstance))
+			return 0;
+
+		// Insert this DLL into the resource chain
+		// NOTE: If this Extension DLL is being implicitly linked to by
+		//  an MFC Regular DLL (such as an ActiveX Control)
+		//  instead of an MFC application, then you will want to
+		//  remove this line from DllMain and put it in a separate
+		//  function exported from this Extension DLL.  The Regular DLL
+		//  that uses this Extension DLL should then explicitly call that
+		//  function to initialize this Extension DLL.  Otherwise,
+		//  the CDynLinkLibrary object will not be attached to the
+		//  Regular DLL's resource chain, and serious problems will
+		//  result.
+
+		new CDynLinkLibrary(MiraledonDLL);
+
+		// Sockets initialization
+		// NOTE: If this Extension DLL is being implicitly linked to by
+		//  an MFC Regular DLL (such as an ActiveX Control)
+		//  instead of an MFC application, then you will want to
+		//  remove the following lines from DllMain and put them in a separate
+		//  function exported from this Extension DLL.  The Regular DLL
+		//  that uses this Extension DLL should then explicitly call that
+		//  function to initialize this Extension DLL.
+		if (!AfxSocketInit())
+		{
+			return FALSE;
+		}
+	}
+	else if (dwReason == DLL_PROCESS_DETACH)
+	{
+		TRACE0("Miraledon.DLL Terminating!\n");
+
+		// Terminate the library before destructors are called
+		AfxTermExtensionModule(MiraledonDLL);
+	}
+	return 1;   // ok
+}
+
+#ifdef _MANAGED
+#pragma managed(pop)
+#endif
