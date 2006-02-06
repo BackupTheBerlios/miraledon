f5e8dc9bf70d00e0d7d1f3db0346ecef9e35b881
gerrit-albrecht <gerrit-albrecht>
2006-02-06 15:22:27 +0000

    *** empty log message ***

--- Miraledon/Resource.h
+++ Miraledon/Resource.h
@@ -3,7 +3,12 @@
 // Used by Miraledon.rc
 //
 #define IDP_SOCKETS_INIT_FAILED         101
-#define IDD_MIRADIALOG                  1007
+#define IDC_PROGRESS1                   1000
+#define IDC_PROGRESS                    1000
+#define IDC_STATIC_ACTION               1001
+#define IDC_STATIC_PERCENT              1002
+#define IDC_ANIMATION                   1003
+#define IDD_MIRA_PROGRESS_DIALOG        1007
 
 // Next default values for new objects
 // 
@@ -11,7 +16,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        1000
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           1008
 #endif
 #endif
---
+++ Miraledon/src/MiraProgressDialog.h
@@ -0,0 +1,50 @@
+// $Id: MiraProgressDialog.h,v 1.1 2006/02/06 15:22:28 gerrit-albrecht Exp $
+//
+// Miraledon Class Library
+// Copyright (C) 2005, 2006 by Gerrit M. Albrecht
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+/// \file MiraProgressDialog.cpp
+/// \author Gerrit M. Albrecht
+/// \brief Contains the declaration of the CMiraProgressDialog class.
+
+#pragma once
+
+#include "MiraDialog.h"
+
+/// \brief This class implements a progress dialog.
+///
+/// CMiraProgressDialog is an extension for the CMiraDialog class.
+/// It dispalys a progress bar, an animation, a current action text
+/// and an percent value for the progress. This dialog contains only
+/// a cancel button.
+
+class CMiraProgressDialog : public CMiraDialog
+{
+  DECLARE_DYNAMIC(CMiraProgressDialog)
+
+  public:
+    /// Standard constructor.
+    CMiraProgressDialog(UINT idd, CWnd *parent = NULL);
+
+    /// Destructor.
+    virtual ~CMiraProgressDialog();
+
+  protected:
+    virtual void DoDataExchange(CDataExchange* pDX);
+    DECLARE_MESSAGE_MAP()
+};
