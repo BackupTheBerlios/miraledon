e520e11e1cbb2cb624f3664dafe342e845e236b9
gerrit-albrecht <gerrit-albrecht>
2006-02-08 09:20:56 +0000

    *** empty log message ***

---
+++ Miraledon/SliderCtrl.cpp
@@ -0,0 +1,42 @@
+// $Id: SliderCtrl.cpp,v 1.1 2006/02/08 09:20:56 gerrit-albrecht Exp $
+//
+// Miraledon Class Library
+// Copyright (C) 2005, 2006 by Gerrit M. Albrecht
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+/// \file SliderCtrl.cpp
+/// \author Gerrit M. Albrecht
+/// \brief Contains the definition of the MSliderCtrl class.
+
+#include "StdAfx.h"
+#include "SliderCtrl.h"
+
+IMPLEMENT_DYNAMIC(MSliderCtrl, CSliderCtrl)
+
+BEGIN_MESSAGE_MAP(MSliderCtrl, CSliderCtrl)
+END_MESSAGE_MAP()
+
+MSliderCtrl::MSliderCtrl()
+{
+#ifndef _WIN32_WCE
+  EnableActiveAccessibility();
+#endif
+}
+
+MSliderCtrl::~MSliderCtrl()
+{
+}
---
+++ Miraledon/SliderCtrl.h
@@ -0,0 +1,44 @@
+// $Id: SliderCtrl.h,v 1.1 2006/02/08 09:20:56 gerrit-albrecht Exp $
+//
+// Miraledon Class Library
+// Copyright (C) 2005, 2006 by Gerrit M. Albrecht
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+/// \file SliderCtrl.cpp
+/// \author Gerrit M. Albrecht
+/// \brief Contains the declaration of the MSliderCtrl class.
+
+#pragma once
+
+/// \brief This class extends the MFC CSliderCtrl class.
+///
+/// MSliderCtrl is an extension for the MFC CSliderCtrl class.
+
+class MSliderCtrl : public CSliderCtrl
+{
+  DECLARE_DYNAMIC(MSliderCtrl)
+
+  public:
+    /// Standard constructor.
+    MSliderCtrl();
+
+    /// Destructor.
+    virtual ~MSliderCtrl();
+
+  protected:
+    DECLARE_MESSAGE_MAP()
+};
