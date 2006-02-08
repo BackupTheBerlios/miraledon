b61347ca51472b770c2797199ee3ebe500565957
gerrit-albrecht <gerrit-albrecht>
2006-02-08 10:04:13 +0000

    *** empty log message ***

--- Resource.h
+++ Resource.h
@@ -5,18 +5,32 @@
 #define IDP_SOCKETS_INIT_FAILED         101
 #define IDC_PROGRESS1                   1000
 #define IDC_PROGRESS                    1000
+#define IDD_MIRA_OPTIONS_DIALOG         1000
 #define IDC_STATIC_ACTION               1001
+#define IDD_MIRA_SPLASH_DIALOG          1001
 #define IDC_STATIC_PERCENT              1002
+#define IDS_MIRALEDON_HOMEPAGE          1002
 #define IDC_ANIMATION                   1003
+#define IDB_LED_STATUS                  1003
+#define IDI_CONN_CONNECTED              1003
+#define IDC_LIST                        1004
+#define IDI_CONN_DISCONNECTED           1004
+#define IDC_LISTBOX                     1005
+#define IDC_HAND                        1005
+#define IDI_LED_GRAY                    1006
 #define IDD_MIRA_PROGRESS_DIALOG        1007
+#define IDI_LED_GREEN                   1007
+#define IDI_LED_RED                     1008
+#define IDC_CURSOR1                     1009
+#define IDC_NOT_ALLOWED                 1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        1000
+#define _APS_NEXT_RESOURCE_VALUE        1010
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           1008
 #endif
 #endif
